e1978d21904d374ed3b571621e0232df3a0893c5
Xak Developer <xakk.net@gmail.com>
2021-07-12 17:47:26 -0700

    update copyright text

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -11,8 +11,8 @@ class FooterComponent extends React.Component<RouterProps> {
         return (
             <React.Fragment>
                 <footer className="pg-footer">
-                    <span>Powered by</span>
-                    <a href="https://www.openware.com">openware.com</a>
+                    <span>All Rights Reserved</span>
+                    <a href="https://www.mesoex.com">MesoEx.com</a>
                 </footer>
             </React.Fragment>
         );
